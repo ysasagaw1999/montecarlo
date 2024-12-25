d3ab64d78b89d951b6ed3c524de4474e8a78cf93
ysasagaw1999 <153721240+ysasagaw1999@users.noreply.github.com>
2024-12-25 04:11:51 +0000

    third

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ This is a monte carlo simulation code to calculate a solid angle and a distance 
 The position of material is defined by a shortest distance and elevation angle from the center of the fan-shape to generated position.
 
 To calculate the "Solid angle",
-You run ". simulation.sh", then you can get the result with "result.csv".
+You run "./solid", then you can get the result with "result.csv".
 
 To calculate the "Distance",
 You run "./distance", then you can get the result with "distance.txt".
--- distance.cpp
+++ distance.cpp
@@ -3,35 +3,34 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include "geometry.h"
 
 int main()
 {
-    double d = 112.33;      // shortest distance
-    double ang = M_PI * 49.6 / 180; // angle of center
+    double d = -1 * distance;      // shortest distance
     double target[3] = { d / tan(ang), 0, d};
-    double length_start = 35.036;
-    double each_length = 6.399375;
-    double angle_start = -1 * M_PI / 6;
-    double each_angle = M_PI / 24;
-    double distance[16][8];
-    for (int f_ch=0; f_ch < 16; f_ch++)
+    double length_start = min_length;
+    double each_length = (max_length - min_length)/f_ch;
+    double each_angle = (max_angle - min_angle)/r_ch;
+    double distance[f_ch][r_ch];
+    for (int f=0; f < f_ch; f++)
     {
-        double center_r = length_start + (f_ch + 0.5) * each_length;
-        for (int r_ch=0; r_ch<8; r_ch++)
+        double center_r = length_start + (f + 0.5) * each_length;
+        for (int r=0; r<r_ch; r++)
         {
-            double center_th = angle_start + (r_ch + 0.5) * each_angle;
+            double center_th = min_angle + (r + 0.5) * each_angle;
             double center[3] = {center_r * cos(center_th), center_r * sin(center_th), 0};
             double dis[3] = {target[0] - center[0], target[1] - center[1], target[2] - center[2]};
-            distance[f_ch][r_ch] = sqrt(dis[0]*dis[0] + dis[1]*dis[1] + dis[2]*dis[2]);
+            distance[f][r] = sqrt(dis[0]*dis[0] + dis[1]*dis[1] + dis[2]*dis[2]);
         }
     }
 
     std::ofstream opfile("distance.txt");
-    for (int f_ch=0; f_ch < 16; f_ch++)
+    for (int f=0; f < f_ch; f++)
     {
-        for (int r_ch=0; r_ch<8; r_ch++)
+        for (int r=0; r<r_ch; r++)
         {
-            opfile << distance[f_ch][r_ch] << ", ";
+            opfile << distance[f][r] << ", ";
         }
 
         opfile << "\n";
